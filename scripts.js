97c0180ba71b32b2c904c1c3d4ad66d084a09561
Zahra Paliwalla <zahrapaliwalla@Zahras-MacBook-Air.local>
2022-01-31 20:06:22 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// $('#btn').on('click', function () {
+//     $('#one').hide();
+//     $('#two').fadeIn();
+// });
+
+$("#clickme").click(function () {
+    $("#book").show("slow", function () {
+        // Animation complete.
+    });
+});
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
